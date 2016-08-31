8d4c5f540fefc621d57028b89f67825345a202ea
f111fei <mail@xzper.com>
2016-08-31 11:15:23 +0800

    Fix btmbar position

--- src/app/components/btmbar/Btmbar.tsx
+++ src/app/components/btmbar/Btmbar.tsx
@@ -23,18 +23,18 @@ export default class Btmbar extends React.Component<any, any> {
 
 	public render(): JSX.Element {
 		return (
-			<div id="main-player" className="m-player">
-				<div title="上一首" className="btnc btnc-prv">
+			<div id="main-player" className="m-player f-os">
+				<div title="上一首" className="btnc btnc-prv f-vc f-cp">
 					<SVGButton href={svg.prev}/>
 				</div>
-				<div title="播放" className="btnp btnp-play">
+				<div title="播放" className="btnp btnp-play f-vc f-cp">
 					<SVGButton href={svg.play}/>
 				</div>
-				<div title="上一首" className="btnc btnc-nxt">
+				<div title="上一首" className="btnc btnc-nxt f-vc f-cp">
 					<SVGButton href={svg.next}/>
 				</div>
 				<time className="now">00: 00</time>
-				<div className="prg prg-ply">
+				<div className="prg prg-ply f-vc">
 					<span className="track"></span>
 					<span className="download" style={{ width: "0%" }}></span>
 					<span className="has" style={{ width: "0%" }}>
@@ -42,10 +42,10 @@ export default class Btmbar extends React.Component<any, any> {
 					</span>
 				</div>
 				<time className="all">00:00</time>
-				<div className="spk" title="静音">
+				<div className="spk f-vc f-cp" title="静音">
 					<SVGButton href={svg.voice}/>
 				</div>
-				<div title="音量调节" className="prg prg-spk">
+				<div title="音量调节" className="prg prg-spk f-vc">
 					<span className="track"></span>
 					<div className="hastrack">
 						<span className="has" style={{ width: "0%" }}></span>
@@ -54,18 +54,18 @@ export default class Btmbar extends React.Component<any, any> {
 						<span className="ctrl"></span>
 					</div>
 				</div>
-				<div className="type">
+				<div className="type f-vc f-cp">
 					<SVGButton href={svg.order}/>
 				</div>
-				<div className="word" title="打开歌词">
+				<div className="word f-vc f-cp" title="打开歌词">
 					<SVGButton href={svg.lrc}/>
 				</div>
-				<div className="list" title="打开播放列表">
-					<span className="icon">
+				<div className="list f-vc f-cp" title="打开播放列表">
+					<span className="icon f-fl">
 						<SVGButton href={svg.state_playlist}/>
 					</span>
-					<span className="num">500</span>
-					<div className="u-arrlay-dn m-queuenotify u-arrlay-opacity"></div>
+					<span className="num f-fl f-ff1">500</span>
+					<div className="u-arrlay u-arrlay-dn m-queuenotify u-arrlay-opacity f-dn">已添加到播放列表</div>
 				</div>
 			</div>
 		);
